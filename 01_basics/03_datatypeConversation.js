9ee059d1da3f44e17b58c831ac563f062b8bb62b
Akshaychaturvedi <92217888+3007akshay@users.noreply.github.com>
2024-09-22 08:19:28 +0000

    datatype conversion

---
+++ 01_basics/03_datatypeConversation.js
@@ -0,0 +1,20 @@
+let isLoggedIn = 1
+ let booleanLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanLoggedIn);
+
+
+ let number = 35;
+ let stringNumber = String(number)
+ //console.log(stringNumber)
+// console.log(typeof stringNumber);
+ 
+
+//**********************Operations*************************** */
+console. log(null >= 0);
+console. log(null == 0);
+console. log(null <= 0);
+console. log (undefined == 0);
+
+console. log (undefined >= 0);
+
+console. log (undefined <= 0);
