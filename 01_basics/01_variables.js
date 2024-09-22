f12d70ee29268ae110e70867baba5842e41e2939
Akshaychaturvedi <92217888+3007akshay@users.noreply.github.com>
2024-09-22 06:35:32 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12553
+let accountEmail = "akshay@gmail.com"
+var accoountPassword = "987654321"
+accountCity = "Mathura"
+
+
+accountEmail = "chaturvedi@gmail.com"
+accoountPassword = "876543"
+accountCity = "Nodia"
+
+
+/* 
+never use var 
+because of issue in block scope and functional scope
+*/
+console.table([
+    accountId, accountEmail, accoountPassword, accountCity
+])
