99c08c07d437bdbb6143201bffa9ee0d54d9e45e
Akshaychaturvedi <92217888+3007akshay@users.noreply.github.com>
2024-10-03 16:49:57 +0000

    Fumctions

---
+++ 01_basics/String.js
@@ -0,0 +1,11 @@
+const name = "Akshay"
+const count = 30
+
+console.log(`my name is ${name} my count is ${count }`)
+
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()* (max -min+1))+min)
---
+++ 01_basics/makefun.js
@@ -0,0 +1,7 @@
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+
+myFunction();
+
